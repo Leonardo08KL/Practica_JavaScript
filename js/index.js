bca26666377abbfc8c1faeb42e7e449a5dc80ddc
Leonardo08KL <95943337+Leonardo08KL@users.noreply.github.com>
2023-05-06 16:46:00 -0600

    Funcion de borrar una fila

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const title = document.getElementById("title");
-    const description = document.getElementById("description");
-    const table = document.getElementById("table");
-    const alert = document.getElementById("alert");
-    const btn = document.getElementById("add");
+document.addEventListener('DOMContentLoaded', function () {
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const table = document.getElementById('table');
+    const alert = document.getElementById('alert');
+    const btn = document.getElementById('add');
+    let id = 1;
   
+      function removeTodo(id){
+          document.getElementById(id).remove();
+      }
     function addTodo() {
-      if (title.value === "" || description.value === "") {
+      if (title.value === '' || description.value === '') {
         alert.classList.remove("d-none");
         alert.innerText = "Title and descriotion are required";
         return;
       }
   
-      alert.classList.add("d-none");
+      alert.classList.add('d-none');
       const row = table.insertRow();
+      row.setAttribute('id', id++);
       row.innerHTML = `
           <td>${title.value}</td>    
           <td>${description.value}</td>
@@ -24,11 +29,17 @@ document.addEventListener("DOMContentLoaded", function () {
               <button class="btn btn-primary mb-1">
               <i class="fa fa-pencil"></i>
               </button>
-              <button class="btn btn-danger mb-1 ml-1">
-              <i class="fa fa-trash"></i>
-              </button>
+              
           </td>
           `;
+  
+         const removeBtn = document.createElement('button');
+         removeBtn.classList.add('btn', 'btn-danger', 'mb-1');
+         removeBtn.innerHTML = '<i class="fa fa-trash"></i>';
+         removeBtn.onclick = function (e) {
+          removeTodo(row.getAttribute('id'));
+         }
+         row.children[3].appendChild(removeBtn);
     }
   
     btn.onclick = addTodo;
