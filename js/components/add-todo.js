885a0f5bed22f8a50813e212a7dd38070c5b5645
Leonardo08KL <95943337+Leonardo08KL@users.noreply.github.com>
2023-05-06 18:55:32 -0600

    Validar Completed

---
+++ js/components/add-todo.js
@@ -0,0 +1,17 @@
+export default class AddTodo {
+  constructor() {
+    this.title = document.getElementById("title");
+    this.description = document.getElementById("description");
+    this.btn = document.getElementById("add");
+  }
+  
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if (!this.title.value || !this.description.value) {
+        this.alert.show("Title and description are required!");
+      } else {
+        callback(this.title.value, this.description.value);
+      }
+    }
+  }
+}
